1ca709e6aef55bcd403946bb1394b49d0636e543
nickenickenicke <144111731+nickenickenicke@users.noreply.github.com>
2024-01-24 14:59:43 +0100

    Added more players

--- main.js
+++ main.js
@@ -3,21 +3,56 @@ const cors = require("cors");
 const app = express();
 const port = 3000;
 
-app.listen(port, () => {
-  console.log(`Listening on port ${port}!`);
-});
-
 app.use(cors());
 
 const players = [
   {
     id: 1,
-    name: "Foppa",
-    jersey: 123,
+    name: "Peter Forsberg",
+    jersey: 111,
     team: "Colorado",
     position: "Forward",
   },
+  {
+    id: 2,
+    name: "Nicklas Lidström",
+    jersey: 666,
+    team: "Detroit Red Wings",
+    position: "Forward",
+  },
+  {
+    id: 3,
+    name: "Mats Sundin",
+    jersey: 999,
+    team: "Toronto Maple Leafs",
+    position: "Forward",
+  },
+  {
+    id: 4,
+    name: "Börje Salming",
+    jersey: 123,
+    team: "Toronto Maple Leafs",
+    position: "Forward",
+  },
+  {
+    id: 5,
+    name: "Henrik Lundqvist",
+    jersey: 0,
+    team: "New York Rangers",
+    position: "Goalie",
+  },
+  {
+    id: 6,
+    name: "Daniel Alfredsson",
+    jersey: 10,
+    team: "Ottawa Senators",
+    position: "Oklart",
+  },
 ];
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}!`);
+});
 
 app.get("/", (req, res) => {
   res.send("Här är ett api som funkar");
