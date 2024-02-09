db5a30e16b34adcb084328343ee1adb26550ffe3
nickenickenicke <144111731+nickenickenicke@users.noreply.github.com>
2024-02-09 15:08:05 +0100

    :coffin: Code cleanup

--- main.js
+++ main.js
@@ -9,51 +9,6 @@ const { sequelize, Player } = require("./models");
 app.use(cors());
 app.use(bodyParser.json());
 
-// const players = [
-//   {
-//     id: 1,
-//     name: "Peter Forsberg",
-//     jersey: 111,
-//     team: "Colorado",
-//     position: "Forward",
-//   },
-//   {
-//     id: 2,
-//     name: "Nicklas Lidström",
-//     jersey: 666,
-//     team: "Detroit Red Wings",
-//     position: "Forward",
-//   },
-//   {
-//     id: 3,
-//     name: "Mats Sundin",
-//     jersey: 999,
-//     team: "Toronto Maple Leafs",
-//     position: "Forward",
-//   },
-//   {
-//     id: 4,
-//     name: "Börje Salming",
-//     jersey: 123,
-//     team: "Toronto Maple Leafs",
-//     position: "Forward",
-//   },
-//   {
-//     id: 5,
-//     name: "Henrik Lundqvist",
-//     jersey: 444,
-//     team: "New York Rangers",
-//     position: "Goalie",
-//   },
-//   {
-//     id: 6,
-//     name: "Daniel Alfredsson",
-//     jersey: 10,
-//     team: "Ottawa Senators",
-//     position: "Defence",
-//   },
-// ];
-
 app.listen(port, () => {
   console.log(`Listening on port ${port}!`);
 });
@@ -63,21 +18,11 @@ app.get("/", (req, res) => {
 });
 
 app.get("/players", async (req, res) => {
-  // res.json(players);
-
   const players = await Player.findAll();
   res.json(players);
 });
 
 app.put("/players/:playerId", async (req, res) => {
-  // let player = players.find((players) => players.id == req.params.playerId);
-  // if (player) {
-  //   player.name = req.body.name;
-  //   player.jersey = req.body.jersey;
-  //   player.position = req.body.position;
-  //   res.status(204).send("");
-  // }
-
   const playerId = parseInt(req.params.playerId) || 0;
   const thePlayer = await Player.findOne({
     where: { id: playerId },
@@ -98,14 +43,6 @@ app.put("/players/:playerId", async (req, res) => {
 });
 
 app.post("/players", async (req, res) => {
-  // console.log(req.body);
-  // players.push({
-  //   id: players.length,
-  //   name: req.body.name,
-  //   jersey: req.body.jersey,
-  //   team: req.body.team,
-  //   position: req.body.position,
-  // });
   await Player.create({
     name: req.body.name,
     jersey: parseInt(req.body.jersey) || 0,
